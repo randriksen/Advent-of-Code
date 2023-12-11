169c9410f74397982ca951033833a4760ef96e43
Ole <ole@rand-hendriksen.net>
2023-12-11 20:19:55 +0100

    day 4 solution

--- 2023/4/scratch.ps1
+++ 2023/4/scratch.ps1
@@ -2,11 +2,29 @@ $inputdata = get-content .\input.txt
 
 $results = @()
 
+# a dictionary that has a number as an id and an object as a value
+$cards = @{}
+
+
+
 $line = $inputdata[0]
 foreach ($line in $inputdata) {
-    $cardnumber = $line.split(":")[0].replace("   ",",").split(",")[1]
+    $cardnumber = $line.split(":")[0].replace("   "," ").replace("  "," ").split(" ")[1]
     $winners = $line.split(":")[1].split("|")[0].trim().replace("  "," ").split(" ")
     $numbers = $line.split(":")[1].split("|")[1].trim().replace("  "," ").split(" ")
+
+    # a card object that contains has the
+    $card = [PSCustomObject]@{
+        cardnumber = $cardnumber
+        winners = $winners
+        numbers = $numbers
+        count = 1
+    }
+    #put card into dictionary
+    #write-host $card
+    $cards.Add($cardnumber,$card)
+    
+
 
     $match = 0
     foreach ($n in $numbers) {
@@ -30,3 +48,29 @@ foreach ($r in $results) {
     $first += $r
 }
 $first
+
+for ($i = 1; $i -lt $inputdata.Count+1; $i++) {
+    $card = $cards["$i"]
+
+    $match = 0
+   # write-host "card: $($card.cardnumber) i: $i"
+    foreach ($n in $card.numbers) {
+        if ($card.winners -contains $n) {
+            $match++
+        }
+    }
+    if ($match -gt 0) {
+        for ($j = 1;$j -le $match; $j++){
+            $tmp =[int]$card.cardnumber + $j
+            $cards["$tmp"].count = $cards["$tmp"].count + $card.count
+           # write-host $cards[($card.cardnumber + $j)]
+        }
+        
+       
+    }
+}
+$second = 0
+foreach ($c in $cards.Values) {
+    $second += $c.count
+}
+$second
